41ed9699f8fe187cc83aa9428df88eaa907e5f63
theglad-x <ugerondu@gmail.com>
2024-07-31 20:33:04 +0000

    updated

--- main.tf
+++ main.tf
@@ -1,50 +1,114 @@
 provider "aws" {
-    region = "us-east-1" 
+  region = "us-east-1"
 }
 
-variable "cidr_blocks" {
-  description = "All cidr blocks and name tags for vpc and subnet"
-  default = "10.0.10.0/24"
-  type = list(object({
-    cidr_block = string
-    name = string
-  }))
+variable "vpc-cidr-block" {}
+variable "subnet-cidr-blocks" {}
+variable "a-zone" {}
+variable "env-prefix" {}
+variable "my-ip" {}
+variable "instance-type" {}
+variable "my-public-key" {}
+
+resource "aws_vpc" "mydev-vpc" {
+  cidr_block = var.vpc-cidr-block
+  tags = {
+    Name = "${var.env-prefix}-vpc"
+  }
 }
 
-resource "aws_vpc" "dev-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
-    tags = {
-        Name = var.cidr_blocks[0].name
-    }
+resource "aws_subnet" "mydev-subnet-1" {
+  cidr_block        = var.subnet-cidr-blocks[0]
+  vpc_id            = aws_vpc.mydev-vpc.id
+  availability_zone = var.a-zone[0]
+  tags = {
+    Name = "${var.env-prefix}-subnet-1"
+  }
+}
+resource "aws_route_table" "mydev-route-able" {
+  vpc_id = aws_vpc.mydev-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mydev-igw.id
+  }
+  tags = {
+    Name = "${var.env-prefix}-rtb"
+  }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id = aws_vpc.dev-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "us-east-1a"
-    tags = {
-        Name = var.cidr_blocks[1].name
-    }
+resource "aws_internet_gateway" "mydev-igw" {
+  vpc_id = aws_vpc.mydev-vpc.id
+  tags = {
+    Name = "${var.env-prefix}-igw"
+  }
 }
 
-data "aws_vpc" "existing-vpc" {
-    default = true
+
+resource "aws_route_table_association" "mydev-rtb-subnet" {
+  subnet_id      = aws_subnet.mydev-subnet-1.id
+  route_table_id = aws_route_table.mydev-route-able.id
 }
 
-resource "aws_subnet" "dev-subnet-2" {
-    vpc_id = data.aws_vpc.existing-vpc.id
-    cidr_block = var.cidr_blocks[2].cidr_block
-    availability_zone = "us-east-1b"
-    tags = {
-      Name = var.cidr_blocks[2].name
-    }
+resource "aws_security_group" "mydev-sg" {
+  name   = "mydev-sg"
+  vpc_id = aws_vpc.mydev-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.my-ip]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my-ip]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env-prefix}-sg"
+  }
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.dev-vpc.id
+data "aws_ami" "latest-ubuntu-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3*"]
+  }
 }
 
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+output "aws_ami" {
+  value = data.aws_ami.latest-ubuntu-image
 }
 
+resource "aws_key_pair" "tf-ssh-key-pair" {
+  key_name   = "glad-keypair"
+  public_key = file(var.my-public-key)
+}
+
+resource "aws_instance" "mydev-server" {
+  ami           = data.aws_ami.latest-ubuntu-image.id
+  instance_type = var.instance-type
+
+  subnet_id              = aws_subnet.mydev-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.mydev-sg.id]
+  availability_zone      = var.a-zone[0]
+
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.tf-ssh-key-pair.key_name
+
+  tags = {
+    Name = "${var.env-prefix}-server"
+  }
+}
